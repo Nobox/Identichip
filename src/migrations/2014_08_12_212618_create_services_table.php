5110a92297752835ec330a9a2295266c030cd008
Alberto Estrada Puerta <albertoe@gmail.com>
2014-08-12 17:58:27 -0400

    Fixed services table migration parse error

--- src/migrations/2014_08_12_212618_create_services_table.php
+++ src/migrations/2014_08_12_212618_create_services_table.php
@@ -24,7 +24,7 @@ class CreateServicesTable extends Migration {
             $table->foreign('user_id')
                 ->references('id')->on('users')
                 ->onDelete('cascade');
-        }
+        });
     }
 
     /**
