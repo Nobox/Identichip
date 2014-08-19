e8c76235c81691800294d3a0429694371a409704
Alberto Estrada Puerta <albertoe@gmail.com>
2014-08-19 15:31:19 -0400

    Moved Models folder

---
+++ src/Nobox/Identichip/Models/Service.php
@@ -0,0 +1,23 @@
+<?php
+namespace Nobox\Identichip\Models;
+
+
+Class Service extends \Eloquent {
+    /**
+     * The database table used by the model.
+     *
+     * @var string
+     */
+    protected $table = 'services';
+
+    protected $fillable = array(
+        'service_id',
+        'name',
+    );
+    public $timestamps = false;
+
+    public function user()
+    {
+        return $this->belongsTo('user');
+    }
+}
